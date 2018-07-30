df06a81f2e6b328cdc732a8c9f29946c682574f6
James <jamescook.106@gmail.com>
2018-07-30 13:50:05 +0100

    Upgraded script with parallelism and better star mass estimate

--- YTAnalysis/master.py
+++ YTAnalysis/master.py
@@ -59,6 +59,8 @@ max_rho_pos_y_data = []
 star_mass_center_x=[]
 star_mass_center_y=[]
 star_mass_center_average=[]
+star_size_x_precise = []
+star_size_y_precise = []
 
 # Other factors
 total_box_size = float(ds[0].domain_right_edge[0])
@@ -113,11 +115,14 @@ for i in ds:
     Mass_Total_data.append(Mass_Total)
 
     program_runtime_data.append(time.time()-i_start)
-    np.savetxt('program_runtime_data.out',program_runtime_data)
-    np.savetxt('L2M.out',L2M_data)
-    np.savetxt('L2H.out',L2H_data)
-    np.savetxt('RhoJam_Average.out',RhoJam_Average_data)
-    np.savetxt('Mass_Total.out',Mass_Total_data)
+
+    if yt.is_root():
+        np.savetxt('program_runtime_data.out',program_runtime_data)
+        np.savetxt('L2M.out',L2M_data)
+        np.savetxt('L2H.out',L2H_data)
+        np.savetxt('RhoJam_Average.out',RhoJam_Average_data)
+        np.savetxt('Mass_Total.out',Mass_Total_data)
+        np.savetxt('time_data.out',time_data)
 
     # Look at initial Stars
     # Line out from 0 to center (x), at the center of (y and z)
@@ -135,6 +140,9 @@ for i in ds:
     x_max_val = x[x_max]
     max_rho_pos_x_data.append(x_max_val)
 
+    if yt.is_root():
+        np.savetxt('max_rho_pos_x_data.out',max_rho_pos_x_data)
+
     # Create a function that goes to negative when we are below 95% of rho max
     rhofun = interp1d(x, rho-0.05*rho_max, kind=Quality, fill_value="extrapolate")
 
@@ -146,123 +154,147 @@ for i in ds:
     # Size of the 95% rho in center
     size_x = (x_2-x_1)/2.
     star_size_initial_x_data.append(size_x)
+    if yt.is_root():
+        np.savetxt('star_size_inimax_rho_postial_x.out',star_size_initial_x_data)
 
-    np.savetxt('star_size_inimax_rho_postial_x.out',star_size_initial_x_data)
+    if x_max_val>center-3.5 and x_max_val<center+3.5:
+        c_precise = i.ray((0,center,center),(total_box_size,center,center))
+        x_precise = c_precise["x"]
+        rho_precise = c_precise["rho"]
+        rho_max_precise = float(rho_precise.max())
+        x_max_precise = np.where(rho_precise == rho_max_precise)[0][0]
+        x_max_val_precise = x_precise[x_max_precise]
+        rhofun_precise = interp1d(x_precise, rho_precise-0.05*rho_max_precise, kind=Quality, fill_value="extrapolate")
+        x_1_precise = fsolve(rhofun_precise,float(x_precise[x_max_precise])-1)[0]
+        x_2_precise = fsolve(rhofun_precise,float(x_precise[x_max_precise])+1)[0]
+        size_x_precise = (x_2_precise-x_1_precise)/2.
+        star_size_x_precise.append(size_x_precise)
 
-    cy = i.ray((x_max_val,0,center),(x_max_val,total_box_size,center))
+        cy_precise = i.ray((x_max_val_precise,0,center),(x_max_val_precise,total_box_size,center))
+        y_precise = cy_precise["y"]
+        rhoy_precise = cy_precise["rho"]
+        rho_max_precise = float(rhoy_precise.max())
+        y_max_precise = np.where(rhoy_precise == rho_max_precise)[0][0]
+        y_max_val_precise = y_precise[y_max_precise]
+        rhofuny_precise = interp1d(y_precise, rhoy_precise-0.05*rho_max_precise, kind=Quality, fill_value="extrapolate")
+        y_1_precise = fsolve(rhofuny_precise,float(y_precise[y_max_precise])-1)[0]
+        y_2_precise = fsolve(rhofuny_precise,float(y_precise[y_max_precise])+1)[0]
+        size_y_precise = (y_2_precise-y_1_precise)/2.
+        star_size_y_precise.append(size_y_precise)
 
-    y = cy["y"]
-    rhoy = cy["rho"]
+        sp = i.sphere([float(x_max_val_precise), float(y_max_val_precise), center], size_x_precise)
+        RhoJamTotalx = float(sp.mean("RhoJam", weight="cell_volume"))*(4./3.)*np.pi*np.power((size_x_precise),3)
+        star_mass_center_x.append(RhoJamTotalx)
+        sp2 = i.sphere([float(x_max_val_precise), float(y_max_val_precise), center], size_y_precise)
+        RhoJamTotaly = float(sp2.mean("RhoJam", weight="cell_volume"))*(4./3.)*np.pi*np.power((size_y_precise),3)
+        star_mass_center_y.append(RhoJamTotaly)
 
-    rhoy_max = float(rhoy.max())
+        if yt.is_root():
+            plt.figure(figsize=(20,20))
+            plt.scatter(x_precise,rho_precise, alpha = alp,label = "t ="+str(i.current_time))
+            plt.scatter(y_precise,rhoy_precise, alpha = alp,label = "t ="+str(i.current_time))
+            plt.xlim([x_1-10,x_2+10])
+            #plt.ylim([-0.0005,0.008])
+            plt.legend(loc = "upper right")
+            plt.xlabel(r'$x~[1/m]$')
+            plt.ylabel(r'$\rho~[M_{pl}^2 m^2]$')
+            plt.savefig(("rho_size/rho%05d.png" % counter),bbox_inches = 'tight')
+            plt.close()
 
-    rhoyfun = interp1d(y, rhoy-0.05*rhoy_max, kind=Quality)
-    y_1 = fsolve(rhoyfun,center-1)[0]
-    y_2 = fsolve(rhoyfun,center+1)[0]
-
-    size_y = (y_2-y_1)/2.
-    SizeyData.append(size_y)
-
-    plt.figure(figsize=(20,20))
-    ax1 = plt.subplot(211)
-    plt.scatter(x,rho, alpha = alp,label = "t ="+str(i.current_time))
-    plt.xlim([x_1-10,x_2+10])
-    #plt.ylim([-0.0005,0.008])
-    plt.legend(loc = "upper right")
-    plt.xlabel(r'$t~[1/m]$')
-    plt.ylabel(r'$\rho~[M_{pl}^2 m^2]$')
-
-    ax2 = plt.subplot(212)
-    plt.plot(time_data,star_size_initial_x_data, linewidth = line,color = "red", alpha = alp,label = "size = 5% maxRho x dir ")
-    plt.plot(time_data,SizeyData, linewidth = line,linestyle = "--",color = "blue", alpha = alp,label = "size = 5% maxRho y dir ")
-#    plt.xlim([0,400])
-    plt.legend(loc = "lower right")
-    plt.xlabel(r'$t~[1/m]$')
-    plt.ylabel(r'$x~[1/m]$')
-    plt.savefig(("rho_size/rho%05d.png" % counter),bbox_inches = 'tight')
-    plt.close()
-
-    if x_max_val>center-5 and x_max_val<center+5:
-        if size_x<1.0:
-            print size_x
-            star_mass_center_x.append(0.0)
-            star_mass_center_y.append(0.0)
-            star_mass_center_average.append(0.0)
-        elif size_y<1.0:
-            print size_y
-            star_mass_center_x.append(0.0)
-            star_mass_center_y.append(0.0)
-            star_mass_center_average.append(0.0)
-        else:
-            sp = i.sphere([center, center, center], size_x)
-            RhoJamTotalx = float(sp.mean("RhoJam", weight="cell_volume"))*(4./3.)*np.pi*np.power((size_x),3)
-            star_mass_center_x.append(RhoJamTotalx)
-            sp2 = i.sphere([center, center, center], size_y)
-            RhoJamTotaly = float(sp2.mean("RhoJam", weight="cell_volume"))*(4./3.)*np.pi*np.power((size_y),3)
-            star_mass_center_y.append(RhoJamTotaly)
-            star_mass_center_average.append(np.mean([star_mass_center_y,star_mass_center_x]))
     else:
+        star_size_x_precise.append(0.)
+        star_size_y_precise.append(0.)
         star_mass_center_x.append(0.0)
         star_mass_center_y.append(0.0)
-        star_mass_center_average.append(0.0)
 
-# Plots
-# Update rcParams so that we get all axis labelling
-rcParams.update({'figure.autolayout': True})
-rcParams['axes.formatter.limits'] = [-5,5]
+    if yt.is_root():
+        np.savetxt('star_size_x_precise.out',star_size_x_precise)
+        np.savetxt('star_size_y_precise.out',star_size_y_precise)
+        np.savetxt('star_mass_center_x.out',star_mass_center_x)
+        np.savetxt('star_mass_center_y.out',star_mass_center_y)
 
-# Timing Plot
-plt.figure(1)
-plt.title('Program Run Time')
-plt.plot(program_frames,program_runtime_data)
-plt.xlabel('Data Files')
-plt.ylabel('Time $[s]$')
-plt.savefig('pictures_program_time.png')
-plt.close()
+if yt.is_root():
+    # Plots
+    # Update rcParams so that we get all axis labelling
+    rcParams.update({'figure.autolayout': True})
+    rcParams['axes.formatter.limits'] = [-5,5]
 
-# L2H
-plt.figure(2)
-plt.plot(time_data,L2H_data)
-plt.title('$\\mathcal{H}$ vs time')
-plt.ylabel('$\\mathcal{H}$')
-plt.xlabel('Time $[1/m]$')
-plt.savefig('H.png')
-plt.close()
+    # Timing Plot
+    plt.figure(1)
+    plt.title('Program Run Time')
+    plt.plot(program_frames,program_runtime_data)
+    plt.xlabel('Data Files')
+    plt.ylabel('Time $[s]$')
+    plt.savefig('pictures_program_time.png')
+    plt.close()
 
-# L2M
-plt.figure(3)
-plt.plot(time_data,L2M_data)
-plt.title('$\\mathcal{M}$ vs time')
-plt.ylabel('$\\mathcal{M}$')
-plt.xlabel('Time $[1/m]$')
-plt.savefig('M.png')
-plt.close()
+    # L2H
+    plt.figure(2)
+    plt.plot(time_data,L2H_data)
+    plt.title('$\\mathcal{H}$ vs time')
+    plt.ylabel('$\\mathcal{H}$')
+    plt.xlabel('Time $[1/m]$')
+    plt.savefig('H.png')
+    plt.close()
 
-# RhoJam_Average
-plt.figure(4)
-plt.plot(time_data,RhoJam_Average_data)
-plt.title('$\\tilde{\\rho}$ vs time')
-plt.ylabel('$\\tilde{\\rho} \\, [M_{pl}^2m^2]$')
-plt.xlabel('Time $[1/m]$')
-plt.savefig('RhoJam_Average.png')
-plt.close()
+    # L2M
+    plt.figure(3)
+    plt.plot(time_data,L2M_data)
+    plt.title('$\\mathcal{M}$ vs time')
+    plt.ylabel('$\\mathcal{M}$')
+    plt.xlabel('Time $[1/m]$')
+    plt.savefig('M.png')
+    plt.close()
 
-# Total Mass
-plt.figure(5)
-plt.plot(time_data,Mass_Total_data)
-plt.plot(time_data,star_mass_center_y)
-plt.plot(time_data,star_mass_center_x)
-plt.plot(time_data,star_mass_center_average)
-plt.title('$M$ vs time')
-plt.ylabel('$M \\, [M_{pl}^2/m]$')
-plt.xlabel('Time $[1/m]$')
-plt.savefig('Mass_Total.png')
-plt.close()
+    # RhoJam_Average
+    plt.figure(4)
+    plt.plot(time_data,RhoJam_Average_data)
+    plt.title('$\\tilde{\\rho}$ vs time')
+    plt.ylabel('$\\tilde{\\rho} \\, [M_{pl}^2m^2]$')
+    plt.xlabel('Time $[1/m]$')
+    plt.savefig('RhoJam_Average.png')
+    plt.close()
 
-# Max rho pos
-plt.figure(6)
-plt.plot(time_data,max_rho_pos_x_data)
-plt.title('$x,y$ vs time')
-plt.xlabel('Time $[1/m]$')
-plt.savefig('max_rho_pos.png')
-plt.close()
+
+    # Star Size
+    plt.figure(5)
+    plt.plot(time_data,star_size_x_precise, label='x size')
+    plt.plot(time_data,star_size_y_precise, label = 'y size')
+    #plt.plot(time_data,star_size_initial_x_data)
+    plt.title('$r$ vs time')
+    plt.ylabel('$r \\, [1/m]$')
+    plt.xlabel('Time $[1/m]$')
+    plt.legend()
+    plt.savefig('Star_Size.png')
+    plt.close()
+
+    # Star Size Diagnostic
+    plt.figure(6)
+    plt.plot(time_data,star_size_x_precise, label='x size')
+    plt.plot(time_data,star_size_y_precise, label = 'y size')
+    plt.plot(time_data,star_size_initial_x_data, label = 'Inital x')
+    plt.title('$r$ vs time')
+    plt.ylabel('$r \\, [1/m]$')
+    plt.xlabel('Time $[1/m]$')
+    plt.legend()
+    plt.savefig('Star_Size_Diagnostic.png')
+    plt.close()
+
+    # Total Mass
+    plt.figure(7)
+    plt.plot(time_data,Mass_Total_data)
+    plt.plot(time_data,star_mass_center_y)
+    plt.plot(time_data,star_mass_center_x)
+    plt.title('$M$ vs time')
+    plt.ylabel('$M \\, [M_{pl}^2/m]$')
+    plt.xlabel('Time $[1/m]$')
+    plt.savefig('Mass_Total.png')
+    plt.close()
+
+    # Max rho pos
+    plt.figure(8)
+    plt.plot(time_data,max_rho_pos_x_data)
+    plt.title('$x,y$ vs time')
+    plt.xlabel('Time $[1/m]$')
+    plt.savefig('max_rho_pos.png')
+    plt.close()
