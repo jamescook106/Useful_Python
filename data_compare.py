188572bf7a60b1a5707d8370e71b236b3ec0eff1
James Widdicombe <jamescook.106@gmail.com>
2019-12-16 07:26:13 -0800

    Formatting Cleanup using PyFlakes and Black, Python3 Compat (#1)
    * rename.py python3
    
    * Global black reformat, pyflakes and python3 compat

--- data_compare.py
+++ data_compare.py
@@ -1,89 +1,88 @@
 # data_compare.py
 # Last updated 15/08/2018
+# Last Formatted Dec 2019
 # James Widdicombe
 # Simple script to compare data from a serial and parallel
 # run of the same program
 
 # Load the modules
-import os
-import time
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
-
-# Matplotlib Settings
-rcParams.update({'figure.autolayout': True})
-rcParams['axes.formatter.limits'] = [-5,5]
 
 # Input Directory for data files
-serial_directory = 'serial'
-parallel_directory = 'parallel'
+serial_directory = "serial"
+parallel_directory = "parallel"
 
 # Time Data
 # Serial
-serial_time_data = np.genfromtxt(serial_directory + '/' + 'time.out')
+serial_time_data = np.genfromtxt(serial_directory + "/" + "time.out")
 
 # Parallel
-parallel_time_data = np.genfromtxt(parallel_directory + '/' + 'time.out')
+parallel_time_data = np.genfromtxt(parallel_directory + "/" + "time.out")
 
 # Input data have different file lengths
-time_difference_index = min(len(serial_time_data),len(parallel_time_data))
+time_difference_index = min(len(serial_time_data), len(parallel_time_data))
 
 test_1 = 0
 for i in range(time_difference_index):
-	if parallel_time_data[i]-serial_time_data[i] !=0:
-		print i
-		test_1 = 1
-
-if test_1 ==0:
-	print 'Test 1 Passed - Time files align'
+    if parallel_time_data[i] - serial_time_data[i] != 0:
+        print(i)
+        test_1 = 1
+if test_1 == 0:
+    print("Test 1 Passed - Time files align")
 else:
-	print 'Test 1 FAILED - Time files do not align'
-
+    print("Test 1 FAILED - Time files do not align")
 # All input files
 input_name_array = [
-'Weyl4_l2_m0_data.out',
-'Weyl4_l2_m1_data.out',
-'Weyl4_l2_m2_data.out',
-'Weyl4_l2_m1n_data.out',
-'Weyl4_l2_m2n_data.out',
-'Weyl4_l3_m0_data.out',
-'Weyl4_l3_m1_data.out',
-'Weyl4_l3_m2_data.out',
-'Weyl4_l3_m3_data.out',
-'Weyl4_l3_m1n_data.out',
-'Weyl4_l3_m2n_data.out',
-'Weyl4_l3_m3n_data.out',
-'Weyl4_l4_m0_data.out',
-'Weyl4_l4_m1_data.out',
-'Weyl4_l4_m2_data.out',
-'Weyl4_l4_m3_data.out',
-'Weyl4_l4_m4_data.out',
-'Weyl4_l4_m1n_data.out',
-'Weyl4_l4_m2n_data.out',
-'Weyl4_l4_m3n_data.out',
-'Weyl4_l4_m4n_data.out'
+    "Weyl4_l2_m0_data.out",
+    "Weyl4_l2_m1_data.out",
+    "Weyl4_l2_m2_data.out",
+    "Weyl4_l2_m1n_data.out",
+    "Weyl4_l2_m2n_data.out",
+    "Weyl4_l3_m0_data.out",
+    "Weyl4_l3_m1_data.out",
+    "Weyl4_l3_m2_data.out",
+    "Weyl4_l3_m3_data.out",
+    "Weyl4_l3_m1n_data.out",
+    "Weyl4_l3_m2n_data.out",
+    "Weyl4_l3_m3n_data.out",
+    "Weyl4_l4_m0_data.out",
+    "Weyl4_l4_m1_data.out",
+    "Weyl4_l4_m2_data.out",
+    "Weyl4_l4_m3_data.out",
+    "Weyl4_l4_m4_data.out",
+    "Weyl4_l4_m1n_data.out",
+    "Weyl4_l4_m2n_data.out",
+    "Weyl4_l4_m3n_data.out",
+    "Weyl4_l4_m4n_data.out",
 ]
 
-serial_master =[]
+serial_master = []
 parallel_master = []
 
 # Load all spherical harmonics
 for i in input_name_array:
-	serial_master.append(np.genfromtxt(serial_directory + '/' +i, dtype=np.complex128, converters={0: lambda s: complex(s.decode().replace('+-', '-'))}))
-	parallel_master.append(np.genfromtxt(parallel_directory + '/' +i, dtype=np.complex128, converters={0: lambda s: complex(s.decode().replace('+-', '-'))}))
-
-#Check all output numbers are the same
+    serial_master.append(
+        np.genfromtxt(
+            serial_directory + "/" + i,
+            dtype=np.complex128,
+            converters={0: lambda s: complex(s.decode().replace("+-", "-"))},
+        )
+    )
+    parallel_master.append(
+        np.genfromtxt(
+            parallel_directory + "/" + i,
+            dtype=np.complex128,
+            converters={0: lambda s: complex(s.decode().replace("+-", "-"))},
+        )
+    )
+# Check all output numbers are the same
 test_2 = 0
 for i in range(len(serial_master)):
-	for j in range(min(len(serial_master[i]),len(parallel_master[i]))):
-		if serial_master[i][j] - parallel_master[i][j] != 0:
-			print serial_master[i][j] - parallel_master[i][j]
-			test_2 = 1
-
-if test_2 ==0:
-	print 'Test 2 Passed - All output files are the same'
+    for j in range(min(len(serial_master[i]), len(parallel_master[i]))):
+        if serial_master[i][j] - parallel_master[i][j] != 0:
+            print(serial_master[i][j] - parallel_master[i][j])
+            test_2 = 1
+if test_2 == 0:
+    print("Test 2 Passed - All output files are the same")
 else:
-	print 'Test 2 FAILED - All output files are NOT the same'
+    print("Test 2 FAILED - All output files are NOT the same")
--- rename.py
+++ rename.py
@@ -3,34 +3,37 @@
 # A script that copies a series on png and reassigns them into a standard order
 # that can be used with ffmpeg. Then creates a movie from those files
 # Last Updated June 2018
+# Last Formatted Dec 2019
 
 # Import Modules
-import os, sys, shutil
+import os, shutil
 import glob
 
 # Function to count png files
 def PngCounter(str_input_directory):
-    return len(glob.glob1(str_input_directory,"*.png"))
+    return len(glob.glob1(str_input_directory, "*.png"))
+
 
 # Function with custom file output
-def OutputName(int_file_number,str_output_path):
-    if int_file_number<10:
-        return str_output_path+'/00000'+str(int_file_number)+'.png'
-    elif int_file_number<100 and int_file_number>9:
-        return str_output_path+'/0000'+str(int_file_number)+'.png'
-    elif int_file_number<1000 and int_file_number>99:
-        return str_output_path+'/000'+str(int_file_number)+'.png'
-    elif int_file_number<10000 and int_file_number>999:
-        return str_output_path+'/00'+str(int_file_number)+'.png'
-    elif int_file_number<100000 and int_file_number>9999:
-        return str_output_path+'/0'+str(int_file_number)+'.png'
+def OutputName(int_file_number, str_output_path):
+    if int_file_number < 10:
+        return str_output_path + "/00000" + str(int_file_number) + ".png"
+    elif int_file_number < 100 and int_file_number > 9:
+        return str_output_path + "/0000" + str(int_file_number) + ".png"
+    elif int_file_number < 1000 and int_file_number > 99:
+        return str_output_path + "/000" + str(int_file_number) + ".png"
+    elif int_file_number < 10000 and int_file_number > 999:
+        return str_output_path + "/00" + str(int_file_number) + ".png"
+    elif int_file_number < 100000 and int_file_number > 9999:
+        return str_output_path + "/0" + str(int_file_number) + ".png"
+
 
 # Input and Output Directories
-Variable = 'phi'
-prefix = '/'
+Variable = "phi"
+prefix = "/"
 Input_Directory = "YTAnalysis"
-suffix = '.0.png'
-Output_Path = 'Output'
+suffix = ".0.png"
+Output_Path = "Output"
 
 # Make an Output_Path
 if not os.path.exists(Output_Path):
@@ -42,42 +45,51 @@ while_counter = 0
 name_counter = 0
 
 # Rename the png
-while while_counter<Total_Files:
-    if name_counter<10:
-        j = Input_Directory + prefix+'0000' + str(name_counter) + suffix
+while while_counter < Total_Files:
+    if name_counter < 10:
+        j = Input_Directory + prefix + "0000" + str(name_counter) + suffix
         if os.path.isfile(j):
-            print j
-            shutil.copyfile(j,OutputName(while_counter,Output_Path))
-            while_counter+=1
-    elif name_counter>9 and name_counter<100:
-        j = Input_Directory + prefix+'000' + str(name_counter) + suffix
+            print(j)
+            shutil.copyfile(j, OutputName(while_counter, Output_Path))
+            while_counter += 1
+    elif name_counter > 9 and name_counter < 100:
+        j = Input_Directory + prefix + "000" + str(name_counter) + suffix
         if os.path.isfile(j):
-            print j
-            shutil.copyfile(j,OutputName(while_counter,Output_Path))
-            while_counter+=1
-    elif name_counter>99 and name_counter<1000:
-        j = Input_Directory + prefix+'00' + str(name_counter) + suffix
+            print(j)
+            shutil.copyfile(j, OutputName(while_counter, Output_Path))
+            while_counter += 1
+    elif name_counter > 99 and name_counter < 1000:
+        j = Input_Directory + prefix + "00" + str(name_counter) + suffix
         if os.path.isfile(j):
-            print j
-            shutil.copyfile(j,OutputName(while_counter,Output_Path))
-            while_counter+=1
-    elif name_counter>999 and name_counter<10000:
-        j = Input_Directory + prefix+'0' + str(name_counter) + suffix
+            print(j)
+            shutil.copyfile(j, OutputName(while_counter, Output_Path))
+            while_counter += 1
+    elif name_counter > 999 and name_counter < 10000:
+        j = Input_Directory + prefix + "0" + str(name_counter) + suffix
         if os.path.isfile(j):
-            print j
-            shutil.copyfile(j,OutputName(while_counter,Output_Path))
-            while_counter+=1
-    elif name_counter>9999 and name_counter<100000:
-        j = Input_Directory + '/pltstate0' + str(name_counter) + '.3d.hdf5_Slice_z_'+Variable+'.png'
+            print(j)
+            shutil.copyfile(j, OutputName(while_counter, Output_Path))
+            while_counter += 1
+    elif name_counter > 9999 and name_counter < 100000:
+        j = (
+            Input_Directory
+            + "/pltstate0"
+            + str(name_counter)
+            + ".3d.hdf5_Slice_z_"
+            + Variable
+            + ".png"
+        )
         if os.path.isfile(j):
-            print j
-            shutil.copyfile(j,OutputName(while_counter,Output_Path))
-            while_counter+=1
-    name_counter+=1
+            print(j)
+            shutil.copyfile(j, OutputName(while_counter, Output_Path))
+            while_counter += 1
+    name_counter += 1
 
 # Create the movie
 os.chdir(Output_Path)
-os.system('ffmpeg -framerate 2 -i %06d.png -c:v libx264 -r 30 -pix_fmt yuv420p movie.mp4')
-os.chdir('..')
-shutil.copyfile(Output_Path+'/movie.mp4',os.getcwd()+'/'+Variable+'.mp4')
+os.system(
+    "ffmpeg -framerate 2 -i %06d.png -c:v libx264 -r 30 -pix_fmt yuv420p movie.mp4"
+)
+os.chdir("..")
+shutil.copyfile(Output_Path + "/movie.mp4", os.getcwd() + "/" + Variable + ".mp4")
 shutil.rmtree(Output_Path)
